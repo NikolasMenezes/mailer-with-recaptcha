97a366b3a025e05c48f944c69cc2c1f8d00a1610
Nikolas Menezes <nikolasdev.contato@gmail.com>
2024-02-01 19:01:32 -0300

    feat: adding recaptcha middleware

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 const mailer = require('nodemailer');
 
 const app = express();
 
 app.use(express.json());
-// app.use(express.cors());
+app.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
 
 const buildHtmlMessage = require('./message.js')
+
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 
 const transporter = mailer.createTransport({
   host: "smtp.gmail.com",
@@ -19,21 +24,23 @@ const transporter = mailer.createTransport({
   },
 });
 
-app.post('/email', (req, res) => {
-  const { subject } = req.body;
+app.post('/email', recaptcha.middleware.verify, (req, res) => {
+  if (req.recaptcha.error) {
+    return res.status(401).json({ message: 'Recaptcha validation failed' });
+  }
 
   const mailOptions = {
     from: process.env.EMAIL_SENDER,
     to: "nikolasmezes23@gmail.com",
-    subject: subject,
+    subject: "Contato pelo site",
     html: buildHtmlMessage(req.body),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      res.status(500).send(error);
+      return res.status(500).json({ error: error.message });
     } else {
-      res.status(200).send('Email sent: ' + info.response);
+      return res.status(200).json({ success: true, 'message': info.response });
     }
   });
 });
